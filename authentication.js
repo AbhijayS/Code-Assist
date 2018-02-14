d4151aa3ef9026f3e5b89bed94f52e73be1e2a21
Christopher Smith <christopher.smithua@stu.austinisd.org>
2018-02-14 12:26:23 -0600

    Basic form connected with server

--- authentication.js
+++ authentication.js
@@ -16,20 +16,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
-//tell express what to do when the /about route is requested
-app.post('/login', function(req, res){
-    res.setHeader('Content-Type', 'application/json');
-    console.log("test")
-    //mimic a slow network connection
-    // setTimeout(function(){
-
-    //     res.send(JSON.stringify({
-    //         firstName: req.body.firstName || null,
-    //         lastName: req.body.lastName || null
-    //     }));
-
-    // }, 1000)
-
-    //debugging output for the terminal
-    // console.log('you posted: First Name: ' + req.body.firstName + ', Last Name: ' + req.body.lastName);
+app.post('/login.html', function(req, res){
+	res.setHeader('Content-Type', 'application/json');
+	console.log(req.body.email)
+	console.log(req.body.password)
 });
