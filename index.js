722f3ffa6e087140998f086c705720ffbe641e6f
Abhijay Saini <abhijay.saini5c@stu.austinisd.org>
2018-02-15 08:27:29 -0600

    Multiple Client working. Reload issue fixed using socket.io and jquery. Responsive and clean design is next steps

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
+/*
+SOCKET.IO
+*/
+
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+const PORT = 80;
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  console.log('A user connected');
+
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+    console.log('Message: ' + msg);
+  });
+
+  socket.on('disconnect', function(){
+    console.log('User disconnected');
+  });
+});
+
+http.listen(PORT, function(){
+  console.log('listening on 80');
+});
+
+/*
+
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io');
 var bodyParser = require('body-parser');
 
-const PORT = 8080;
-
+EXPRESS SERVER
 //support parsing of application/json type post data
 app.use(bodyParser.json());
 //support parsing of application/x-www-form-urlencoded post data
@@ -19,29 +50,14 @@ app.get('/', function(req, res){
 });
 
 app.post('', function(req, res){
+  //res.redirect(303, '/');
+  //res.end('End');
   console.log('Message: ' + req.body.msg);
+  //io.socket.emit('message', req.body.msg);
+
 })
 
 app.listen(PORT, function() {
-  console.log("Server running on port 8080")
-});
-
-
-/*
-OLD TESTING STUFF
-io.on('connection', function(socket){
-  console.log('A user connected');
-
-  socket.on('chat message', function(msg){
-    console.log('Message: ' + msg);
-  });
-
-  socket.on('disconnect', function(){
-    console.log('User disconnected');
-  });
-});
-
-http.listen(PORT, function(){
-  console.log('listening on 8080');
+  console.log("Server running on port 80")
 });
 */
