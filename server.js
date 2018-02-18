19b8d22cddb08c5189b864831a546bfddf4b0967
Abhijay Saini <abhijay.saini@gmail.com>
2018-02-18 11:08:47 -0600

    revert commit

--- server.js
+++ server.js
@@ -21,19 +21,45 @@ http.listen(PORT, function(){
 const PORT = 80;
 var express = require('express');
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.use(express.static("."));
 
-var server = app.listen(PORT, function() {
-  console.log("Server running on port 80")
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
 });
 
-var socket = require('socket.io');
-var io = socket(server);
+app.get('/chat.html', function(req, res){
+  res.sendFile(__dirname + '/chat.html');
+});
+
+app.get('/login.html', function(req, res){
+  res.sendFile(__dirname + '/login.html');
+});
 
 io.on('connection', function(socket){
+  console.log('User connected');
+
   socket.on("signup", function(user) {
     console.log("Username: " + user.username)
     console.log("Password: " + user.password)
   });
+
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+    console.log('Message: ' + msg);
+  });
+
+  socket.on('disconnect', function(){
+    console.log('User disconnected');
+  });
+
+  socket.on('username', function(data){
+    console.log('Username: '+ data);
+  })
+});
+
+http.listen(PORT, function(){
+  console.log('listening on 80');
 });
