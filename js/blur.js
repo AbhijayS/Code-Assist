d3b0dc3ba66743113f8fc507d7dd7f5e7129149b
Abhijay Saini <abhijay.saini@gmail.com>
2018-02-18 21:18:37 -0600

    login page iframe prototype created. Overflows when shrinked height, otherwise, colors and design are the only thing left

--- js/blur.js
+++ js/blur.js
@@ -1,11 +1,16 @@
 var pageEl = document.getElementById('page');
-var signUpEl = document.getElementById('signUpPage');
+var formEl = document.getElementsByClassName('signIn-form')[0];
 var buttonEl = document.getElementById('signUp');
 var blur = buttonEl.addEventListener("click", function(){
   console.log('cllickd');
-  pageEl.style.backgroundColor = 'lightgrey';
-  pageEl.style.transition = '1s'
-  pageEl.style.opacity = '0.3';
-  signUpEl.style.display = "block";
+  pageEl.style.transition = '2s'
+  pageEl.style.filter = 'blur(10px)';
+  pageEl.style.WebkitFilter = "blur(10px)";
 
+  pageEl.style.transition = '2s'
+  formEl.style.filter = 'blur(10px)';
+  formEl.style.WebkitFilter = "blur(10px)";
+  formEl.style.display = "block";
+  formEl.style.filter = '';
+  formEl.style.WebkitFilter = '';
 })
