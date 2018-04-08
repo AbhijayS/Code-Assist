8a8f38279e9efac745ca83ef329cd75458b7c1a8
Abhijay Saini <abhijay.saini@gmail.com>
2018-04-08 10:46:14 -0500

    Branch renamed

--- routes/index.js
+++ routes/index.js
@@ -23,6 +23,14 @@ router.post('/register', function(req, res){
 	var username = req.body.username;
 	var email = req.body.email;
 	var password = req.body.password;
+	// var CS_Class = req.body.CS_class;
+
+	req.checkBody('username', 'Username is required').len(3);
+	req.checkBody('email', 'Email is required').notEmpty();
+	req.checkBody('email', 'Email is invalid').isEmail();
+	req.checkBody('password', 'Password must be at least 6 characters long').len(6);
+	req.checkBody('password', 'Password can\'t be longer than 128 characters').not().len(128);
+	req.checkBody('password2', 'Passwords do not match').equals(req.body.password);
 
 	var newUser = new User({
 		username: username,
