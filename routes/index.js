1176c33ee9f4b15b41fd6a841e86e57891aab2be
Abhijay Saini <abhijay.saini@gmail.com>
2018-04-08 11:56:36 -0500

    Login system working
    We still have to remove the sign up and login buttons from the pages when user logs in and replace with profile and log out buttons

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 var path = require('path');
 var User = require('../models/user');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 
 // Get Homepage
 router.get('/', function(req, res){
@@ -24,14 +26,14 @@ router.post('/register', function(req, res){
 	var email = req.body.email;
 	var password = req.body.password;
 	// var CS_Class = req.body.CS_class;
-
-	req.checkBody('username', 'Username is required').len(3);
-	req.checkBody('email', 'Email is required').notEmpty();
-	req.checkBody('email', 'Email is invalid').isEmail();
-	req.checkBody('password', 'Password must be at least 6 characters long').len(6);
-	req.checkBody('password', 'Password can\'t be longer than 128 characters').not().len(128);
-	req.checkBody('password2', 'Passwords do not match').equals(req.body.password);
-
+	//
+	// req.checkBody('username', 'Username is required').len(3);
+	// req.checkBody('email', 'Email is required').notEmpty();
+	// req.checkBody('email', 'Email is invalid').isEmail();
+	// req.checkBody('password', 'Password must be at least 6 characters long').len(6);
+	// req.checkBody('password', 'Password can\'t be longer than 128 characters').not().len(128);
+	// req.checkBody('password2', 'Passwords do not match').equals(req.body.password);
+	//
 	var newUser = new User({
 		username: username,
 		email: email,
@@ -47,6 +49,41 @@ router.post('/register', function(req, res){
 
 	});
 	res.redirect('register');
+});
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+		User.getUserByUsername(username, function(err, user) {
+			if(err) throw err;
+			if(!user) {
+				return done(null, false, {message: 'Unknown user'});
+			}
+
+			User.comparePassword(password, user.password, function(err, isMatch) {
+				if(err) throw err;
+				if(isMatch) {
+					return done(null, user);
+				}else{
+					return done(null, false, {message: 'Invalid password'});
+				}
+			});
+		});
+  }));
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.getUserById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+router.post('/login',
+ 	passport.authenticate('local', {failureRedirect: '/login'}),
+	function(req, res) {
+		res.redirect('/');
 });
 
 module.exports = router;
