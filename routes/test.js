adf8c1515632029449f7c9462478218343cdaabf
Chris5407 <christopher.smith4202@gmail.com>
2018-04-15 13:09:55 -0500

    created basic structure of community page with links to seperate post pages

--- routes/test.js
+++ routes/test.js
@@ -4,46 +4,76 @@ var User = require('../models/user');
 // var User = require('../models/test-user');
 
 router.get('/', function(req, res) {
-  var answer1 = new User.AnswerSchema({
-	answer: "MongoDB"
-  });
 
-  answer1.save(function(err) {
-	if(err) throw err;
-  });
-
-  User.PostSchema.find({question: "What?"}).populate('answers').exec(function(err, newPost) {
-	for (var i = 0; i < newPost.length; i++)
-	{
-	  newPost[i].answers.push(answer1);
-
-	  newPost[i].save(function(err) {
-		if(err) throw err;
-		console.log("New Post Saved:")
-		console.log(newPost);
-		console.log('-------------------------');
-		console.log('');
-	  });
-	}
-	User.PostSchema.find({}).populate('answers').exec(function(err, posts) {
-	  if(err) throw err;
-	  console.log(posts);
-	  res.render('community', {layout: false, posts: posts});
+	User.CommunitySchema.findOne({}).populate('posts').exec(function(err, community) {
+		res.render('community', {layout: false, posts: community.posts});
 	});
-  });
+
 });
 
+/*	var answer1 = new User.AnswerSchema({
+	answer: "MongoDB"
+	});
+
+	answer1.save(function(err) {
+	if(err) throw err;
+	});
+
+	User.PostSchema.find({question: "What?"}).populate('answers').exec(function(err, newPost) {
+		for (var i = 0; i < newPost.length; i++)
+		{
+			newPost[i].answers.push(answer1);
+
+			newPost[i].save(function(err) {
+			if(err) throw err;
+			console.log("New Post Saved:")
+			console.log(newPost);
+			console.log('-------------------------');
+			console.log('');
+			});
+		}
+		User.PostSchema.find({}).populate('answers').exec(function(err, posts) {
+			if(err) throw err;
+			console.log(posts);
+			res.render('community', {layout: false, posts: posts});
+		});
+	});*/
+
 router.get('post', function(req, res) {
-  if(req.user)
-  {
+	if(req.user)
+	{
 	res.render('community-post');
-  }else{
+	} else {
 	res.redirect('login');
-  }
+	}
 });
 
 router.post('post', function(req, res) {
-  // var question = req.body.question;
+	// var question = req.body.question;
+});
+
+router.get('/:id', function(req, res) {
+	var postID = req.params.id;
+
+	var newAnswer = new User.AnswerSchema({
+		answer: "MongoDB"
+	});
+	newAnswer.save(function(err) {
+		if(err) throw err;
+	});
+
+	User.PostSchema.findOne({_id: postID}).populate('answers').exec(function(err, post) {
+		console.log(post);
+
+		post.answers.push(newAnswer);
+
+		post.save(function(err) {
+			if(err) throw err;
+		});
+
+		res.render('post', {layout: false, post: post});
+	});
+
 });
 
 module.exports = router;
