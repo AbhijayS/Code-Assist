54b3c8029f69bf55f57fe497c635b383ab6a9ecb
Abhijay Saini <abhijay.saini@gmail.com>
2018-04-16 21:54:51 -0500

    Users can now post and mentors can view all private_posts

--- routes/mentor.js
+++ routes/mentor.js
@@ -62,12 +62,57 @@ router.post('/send', function(req, res) {
 router.get('/history', function(req, res) {
   if(req.user) {
     User.UserSchema.findOne({_id: req.user._id}).populate('private_posts').exec(function(err, user) {
-      res.render('mentor-history', {layout: 'dashboard-layout', posts: user.private_posts})
+      var allPosts = user.private_posts;
+      allPosts.sort(function(date1,date2){
+        if (date1 > date2) return -1;
+        if (date1 < date2) return 1;
+        return 0;
+      });
+      res.render('mentor-history', {layout: 'dashboard-layout', posts: allPosts});
     });
   }else{
     req.flash('origin');
     req.flash('origin', '/mentor/history');
-    res.redirect('../login');
+    res.redirect('../../login');
   }
+});
+
+router.get('/history/:id', function(req, res) {
+  var postID = req.params.id;
+  if(req.user)
+  {
+    var found = false;
+    // console.log('-------------------------');
+    // console.log(req.user.private_posts.length);
+    // console.log(req.user.private_posts);
+    // console.log('');
+    // console.log('');
+    for (var i = 0; (i < req.user.private_posts.length); i++)
+    {
+      console.log("Iterating: " + req.user.private_posts[i]);
+      if(req.user.private_posts[i] == postID)
+      {
+        found = true;
+        break;
+      }
+    }
+
+    if(found)
+    {
+      // console.log("Found: " + found);
+      User.PostSchema.findOne({_id: postID}).populate('answers').exec(function(err, post) {
+        res.render('mentor-history-post', {layout: 'dashboard-layout', post: post});
+      });
+    }else{
+      res.send("You don't have access to this post");
+    }
+  }else{
+    req.flash('origin');
+    req.flash('origin', '/mentor/history/'+postID);
+    res.redirect('../../login');
+  }
+
+  // res.render('mentor-history-post', {layout: 'dashboard-layout', post: post});
+
 });
 module.exports = router;
