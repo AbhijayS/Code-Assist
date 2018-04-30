f8b21bc0a503551b28bf0885d4a1702e8edd2d29
Abhijay Saini <abhijay.saini@gmail.com>
2018-04-30 05:58:22 -0500

    404 page added

--- routes/index.js
+++ routes/index.js
@@ -341,6 +341,10 @@ router.post('/password-change', function(req, res) {
 
 });
 
+//The 404 Route (ALWAYS Keep this as the last route)
+router.get('*', function(req, res){
+  res.render('_404', {layout: 'dashboard-layout'});
+});
 
 /*
 =====================================================
