3312e81d25cd2fce43b9f42dbaf35a2190e5a318
Chris Smith <christopher.smith4202@gmail.com>
2018-05-05 16:05:23 -0500

    Fixed quill borders

--- public/vendors/js/main.js
+++ public/vendors/js/main.js
@@ -6,7 +6,7 @@
     /*==================================================================
     [ Focus input ]*/
     $('.input100').each(function(){
-        $(this).on('blur', function(){
+        $(this).on('change', function(){
             if($(this).val().trim() != "") {
                 $(this).addClass('has-val');
             }
