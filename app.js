12e83442a3e868d898719ce7d6144d1b8c0c17dd
Abhijay Saini <abhijay.saini@gmail.com>
2018-06-26 13:45:42 -0500

    Blog Opt Ins started

--- app.js
+++ app.js
@@ -76,5 +76,5 @@ app.use(function(req, res, next){
 app.set('port', (process.env.PORT || 3000));
 
 app.listen(app.get('port'), function(){
-	// console.log('Server started on port '+app.get('port'));
+	console.log('Server started on port '+app.get('port'));
 });
