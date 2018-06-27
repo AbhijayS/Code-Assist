c65342dee59479b6716e42bfa2292621c09bad54
Abhijay Saini <abhijay.saini@gmail.com>
2018-06-26 23:10:25 -0500

    In the process of designing a drop down for the free pdf

--- app.js
+++ app.js
@@ -73,7 +73,7 @@ app.use(function(req, res, next){
 });
 
 // Set Port
-app.set('port', (process.env.PORT || 3000));
+app.set('port', (process.env.PORT || 8080));
 
 app.listen(app.get('port'), function(){
 	console.log('Server started on port '+app.get('port'));
