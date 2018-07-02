70890b1d7a0679a8c99b5263f62209d319dc31bd
Abhijay Saini <abhijay.saini@gmail.com>
2018-07-01 20:32:40 -0500

    - Side bar "create new project" button created - still need validation

--- public/js/codehat-dash.js
+++ public/js/codehat-dash.js
@@ -1,4 +1,21 @@
 window.onload =
 function() {
-  
+  var sidebar = $('#sidebar');
+  // var sidebar_expand_width = 350;
+  $('#sidebar #create-project').click(function() {
+    $('.sidebar-box').width(0);
+    $('#sidebar li').not($(this)).removeClass('select');
+    $(this).toggleClass('select');
+    $('#sidebar .sidebar-box#create-project-box').width() == 0 ? $('#sidebar .sidebar-box#create-project-box').width(350) : $('#sidebar .sidebar-box#create-project-box').width(0);
+    $('#projects').css('margin-left') == '75px' ? $('#projects').css({'margin-left' : '350px'}) : $('#projects').css({'margin-left' : '75px'});
+
+  });
+
+  $('#sidebar #global-chat').click(function() {
+    $('.sidebar-box').width(0);
+    $('#sidebar li').not($(this)).removeClass('select');
+    $(this).toggleClass('select');
+    $('#projects').css({'margin-left' : '75px'});
+    $('#sidebar .sidebar-box#global-chat-box').width() == 0 ? $('#sidebar .sidebar-box#global-chat-box').width('100%') : $('#sidebar .sidebar-box#global-chat-box').width(0);
+  });
 };
