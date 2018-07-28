1eeb5f46a10be2c19d84aba1e25f02a928014f43
Abhijay Saini <abhijay.saini@gmail.com>
2018-07-27 20:23:17 -0500

    community page completed

--- public/js/community-filter.js
+++ public/js/community-filter.js
@@ -30,9 +30,25 @@ window.onload = function(){
           var answers = data[i].answers.length;
           var author = data[i].author;
           var lang = data[i].prog_lang;
+          // var newPost = `
+          // <a class="list-group-item" href="/community/${id}">${question} <span class="badge progress-bar-danger">${answers}</span> <span class="badge">${author}</span> <span class="badge progress-bar-info">${lang}</span> </a>
+          // `;
           var newPost = `
-          <a class="list-group-item" href="/community/${id}">${question} <span class="badge progress-bar-danger">${answers}</span> <span class="badge">${author}</span> <span class="badge progress-bar-info">${lang}</span> </a>
-          `;
+            <a href="/community/${id}">
+            <li class="list-group-item list-group-item-action lead" style="font-weight: 400;">
+            ${question}
+            <span style="position: absolute; right: 0;">
+            <span class="badge badge-warning badge-pill mr-3">${answers} Answers</span>
+            <span class="badge badge-primary badge-pill mr-3">${lang}</span>
+            </span>
+
+            <div class="">
+            <p class="text-gray pt-4 my-0">${author}</p>
+            </div>
+            </li>
+            </a>
+          `
+
           postsContainer.append(newPost);
         }
 
