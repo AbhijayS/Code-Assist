cba6d9d1749c72af8ee4f604e58723a2ff41c6ee
Chris5407 <christopher.smith4202@gmail.com>
2018-08-06 12:37:39 -0500

    File deletion finished

--- routes/community.js
+++ routes/community.js
@@ -657,6 +657,8 @@ router.post('/post/edit/:id', upload.array('file'), function(req, res) {
 		var author = req.user.username;
 		var prog_lang = req.body.programming;
 		var authorid = req.user._id;
+		var removedFileIds = req.body.removedFileIds;
+		// console.log("removedFileIds:", removedFileIds);
 
 		if (question.trim().split(' ').length < 3) {
 			data.questionInvalid = true;
@@ -667,7 +669,7 @@ router.post('/post/edit/:id', upload.array('file'), function(req, res) {
 				var found = false;
 				var allPosts = user.posts;
 				for(var i = 0; i < allPosts.length; i++) {
-					console.log(allPosts[i]._id);
+					// console.log(allPosts[i]._id);
 					if(allPosts[i]._id == postID)
 						found = true;
 				}
@@ -679,7 +681,35 @@ router.post('/post/edit/:id', upload.array('file'), function(req, res) {
 						post.description = description;
 						post.prog_lang = prog_lang;
 
-						/* Chris please take of the new Files coming in here */
+						for (var i = 0; i < req.files.length; i++) {
+							// console.log(req.files[i]);
+
+							// new file reference
+							var newFileRef = new User.FileRefSchema();
+							newFileRef.name = req.files[i].filename;
+							newFileRef.fileID = req.files[i].id;
+							newFileRef.save(function(err) {
+								if(err) throw err;
+								// saved
+							});
+							post.files.push(newFileRef);
+						}
+
+						if (removedFileIds) {
+							// in case only one file needs to be removed
+							if (!Array.isArray(removedFileIds)) {
+								removedFileIds = [removedFileIds];
+							}
+
+
+							for (var i = 0; i < removedFileIds.length; i++) {
+								console.log("removing file: " + removedFileIds[i])
+								post.files.pull({_id: removedFileIds[i]});
+								User.FileRefSchema.findOneAndRemove({_id: removedFileIds[i]}, function(err, fileRef) {
+									gfs.files.remove({_id: fileRef.fileID});
+								});
+							}
+						}
 
 						post.status.edited = true;
 						post.save(function(err) {
