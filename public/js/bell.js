c2a8226df283f3dad154db6b52c713ff740f9aae
Chris5407 <christopher.smith4202@gmail.com>
2018-08-14 19:09:10 -0500

    notification bell frontend started

---
+++ public/js/bell.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  $('#bell').on('shown.bs.dropdown', function () {
+    console.log("shown");
+  });
+
+  $('#bell').on('hidden.bs.dropdown', function () {
+    console.log("hidden");
+  });
+});
