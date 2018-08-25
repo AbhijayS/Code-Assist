a91518a4d1c6c867402711058d4f4b838fdebff5
Chris5407 <christopher.smith4202@gmail.com>
2018-08-24 22:28:28 -0500

    Changed port back to 8080

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var yes_https = require('yes-https');
 var app = express();
 
 // Set Port
-app.set('port', (process.env.PORT || 8081));
+app.set('port', (process.env.PORT || 8080));
 
 var server = app.listen(app.get('port'), function(){
   // console.log('Server started on port '+app.get('port'));
