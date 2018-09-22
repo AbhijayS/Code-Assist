c1e8e25be90a4f286ecfa1c8bb38da9bb4063e9a
Abhijay Saini <abhijay.saini@gmail.com>
2018-09-22 00:27:56 -0500

    rtc testing server 3001

--- easyrtc/easyrtc_server_setup.js
+++ easyrtc/easyrtc_server_setup.js
@@ -7,7 +7,7 @@ var easyrtc = require('./easyrtc_server');  // EasyRTC external module
 var httpApp = express();
 
 // Start Express http server on port 8888
-var webServer = http.createServer(httpApp).listen(8888);
+var webServer = http.createServer(httpApp).listen(3001);
 
 // Start Socket.io so it attaches itself to Express server
 var socketServer = io.listen(webServer);
