7c82688d1fcbed026f43fa4b90bc935b2fec54eb
Abhijay Saini <abhijay.saini@gmail.com>
2018-09-30 21:13:52 -0500

    public projects started

--- models/user.js
+++ models/user.js
@@ -144,7 +144,9 @@ var ProjectSchema = new Schema({
 	invitationPending: false,
 	mentor_invitation_secret: String,
 
-	status: {type: String, default: "new"} // new, using, unused
+	status: {type: String, default: "new"}, // new, using, unused
+
+	publicProject: false
 });
 
 // Post Schema
